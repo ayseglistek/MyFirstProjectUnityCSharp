dc24aed3e84a7a714fcc7aaf7f865ae208e14ece
DESKTOP-82005RA\Ayşegül <aysegl.istek@gmail.com>
2021-07-30 15:38:42 +0300

    #Lesson1
    - Variable Declaration Types

---
+++ Assets/degiskenler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class degiskenler : MonoBehaviour
+{
+    /*
+    // class içinde değer tanımlama
+    string metnim = "Merhaba Ayşegül";
+    int yas= 24;
+    float kusurat = 3.10f;
+    double kusurat2 = 3.102;
+    bool acikMi = false;
+*/
+    string metnim;
+    int yas;
+    float kusurat;
+    double kusurat2;
+    bool acikMi;
+    void Start()
+    {
+        metnim = "içeride metni yazdık.";
+
+        Debug.Log(metnim);
+        Debug.Log(yas);
+        Debug.Log(kusurat);
+        Debug.Log(kusurat2);
+        Debug.Log(acikMi);
+    }
+    }
+
+    
